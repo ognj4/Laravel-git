d996162b24904feb0abc0875f37f18eecafe3f9c
ognj4 <stepanovicognjenos@gmail.com>
2025-02-05 15:52:30 +0100

    L4-domaci

---
+++ domaci1/app/Models/ProductsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductsModel extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name', 'description','amount','price','image'
+    ];
+}
