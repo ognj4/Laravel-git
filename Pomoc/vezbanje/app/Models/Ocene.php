f3a303e9e85d8e2b019064ca3a794df5c2c5dfd9
ognj4 <stepanovicognjenos@gmail.com>
2025-02-07 20:48:06 +0100

    Predavanje

---
+++ Pomoc/vezbanje/app/Models/Ocene.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ocene extends Model
+{
+    protected $table = 'ocene';
+    protected $fillable = [
+        'predmet', 'ocena', 'profesor',
+    ];
+}
