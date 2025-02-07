25d24b3a9d5d66662a42a8b6c66f846e0b9bf716
ognj4 <stepanovicognjenos@gmail.com>
2025-02-07 11:03:46 +0100

    L6-predavanje-helpTomo

--- vezbanje/database/migrations/2025_02_06_104748_create_ocene_table.php
+++ vezbanje/database/migrations/2025_02_06_104748_create_ocene_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('ocene', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
 
             $table->string('predmet',64);
             $table->unsignedInteger('ocena');
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ocenes');
+        Schema::dropIfExists('ocene');
     }
 };
