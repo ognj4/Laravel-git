378627d7d8eee4f8bf908f8300c4ea89ccabec01
ognj4 <stepanovicognjenos@gmail.com>
2025-02-11 22:23:06 +0100

    Domaci

--- domaci1/routes/web.php
+++ domaci1/routes/web.php
@@ -1,20 +1,42 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/about', 'about');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [\App\Http\Controllers\HomepageController::class,'index']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/shop', [\App\Http\Controllers\ShopController::class,'index']);
+
+Route::get("/contact", [ContactController::class, "index"]);
+
+Route::get('/admin/all-contacts', [ContactController::class, "getAllContacts"]);
+
+
+Route::get('/admin/all-products', [\App\Http\Controllers\ProductsController::class, "index"])
+    ->name('sviProizvodi');
+
+
+Route::get('/admin/delete-product/{product}', [\App\Http\Controllers\ProductsController::class, "delete"])
+    ->name('obrisiProizvod'); // "drugo" ime za rutu koju pozivamo preko route ('') u bladeu
+Route::get('/admin/delete-contact/{contact}', [\App\Http\Controllers\ContactController::class, "delete"])
+    ->name('obrisiKontakt');
+
+
+Route::post("/send-contact", [ContactController::class, "sendContact"]);
+
+// stranica za prikaz forme
+Route::view('/admin/add-product', 'addProduct');
+// stranica za snimanje podataka
+Route::post('/admin/save-product',[\App\Http\Controllers\ProductsController::class,'saveProduct'])
+    ->name('snimanjeOglasa');
+
+// {product} u uri znaci da je to varijabla i nju stavljamo u funckciju u controller-u
+Route::get('/admin/product/edit/{product}',[\App\Http\Controllers\ProductsController::class,'singleProduct'])
+    ->name('product.single');
+Route::post('admin/product/save/{product}',[\App\Http\Controllers\ProductsController::class, 'edit'])
+    ->name('product.save');
+
 
 require __DIR__.'/auth.php';
