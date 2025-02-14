ded2547bdd810795e0e4b6128f834d356dab00bd
ognj4 <stepanovicognjenos@gmail.com>
2025-02-14 12:10:08 +0100

    * Domaci 12    - Added forecast table and model

---
+++ domaci_auth/app/Models/ForecastsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ForecastsModel extends Model
+{
+    protected $table = 'forecasts';
+
+    protected $fillable = [
+      'city_id','temperature','forecast_date'
+    ];
+}
