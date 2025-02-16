c18af6d8c20b47a5bacf49c137c4975a4a6ba8ed
ognj4 <stepanovicognjenos@gmail.com>
2025-02-16 22:45:00 +0100

    * select instead of input

--- domaci_auth/resources/views/admin/weather_index.blade.php
+++ domaci_auth/resources/views/admin/weather_index.blade.php
@@ -1,12 +1,18 @@
+@php use App\Models\CitiesModel;use App\Models\WeatherModel; @endphp
+
 <form>
     <input type="text" name="temperature" placeholder="Unesite temperaturu">
-    <input type="text" name="city_id" placeholder="Unesite ID grada">
+    <select name="city_id">
+        @foreach(CitiesModel::all() as $city)
+            <option value="{{$city->id}}">{{$city->name}}</option>
+        @endforeach
+    </select>
     <button>Snimi</button>
 </form>
 
 <div>
 
-    @foreach(\App\Models\WeatherModel::all() as $weather)
+    @foreach(WeatherModel::all() as $weather)
         <p>
             {{ $weather->city->name }} - {{$weather->temperature}}
         </p>
