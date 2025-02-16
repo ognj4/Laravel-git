371793b72c346e56532ac94f242e36cb3afd8ac8
ognj4 <stepanovicognjenos@gmail.com>
2025-02-16 22:19:46 +0100

    * fillable for ForecastsModel

--- domaci_auth/app/Models/ForecastsModel.php
+++ domaci_auth/app/Models/ForecastsModel.php
@@ -9,9 +9,10 @@ class ForecastsModel extends Model
     protected $table = 'forecasts';
 
     protected $fillable = [
-        'city_id', 'temperature', 'forecast_date'
+        'city_id', 'temperature', 'forecast_date','weather_type', 'probability'
     ];
 
+    const WEATHERS = ['rainy','sunny','snowy'];
     public function city()
     {
         // ovako pravimo one to one relaciju, dajemo mu model iz kog uzimamo foreign
