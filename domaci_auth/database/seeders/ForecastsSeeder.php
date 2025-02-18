bfe5d1892f6745e262c939853f2069d2415ae813
ognj4 <stepanovicognjenos@gmail.com>
2025-02-18 11:24:53 +0100

    * Domaci 15 bonus    - ForecastsSeeder logic for temperature not oscillating

--- domaci_auth/database/seeders/ForecastsSeeder.php
+++ domaci_auth/database/seeders/ForecastsSeeder.php
@@ -18,6 +18,8 @@ class ForecastsSeeder extends Seeder
 
         foreach($cities as $city){
 
+            $lastTemperature = null;
+
             for($i=0; $i < 5; $i++){
 
                 $weatherType = ForecastsModel::WEATHERS[rand(0,3)];
@@ -29,19 +31,25 @@ class ForecastsSeeder extends Seeder
 
                 $temperature= null;
 
-                switch ($weatherType) {
-                    case "sunny":
-                        $temperature = rand(-50,50);
-                        break;
-                    case "cloudy":
-                        $temperature = rand(-50,15);
-                        break;
-                    case 'rainy':
-                        $temperature = rand(-10,50);
-                        break;
-                    case 'snowy':
-                        $temperature = rand(-50,1);
-                        break;
+                if ($lastTemperature !== null) {
+                    $minTemperature = $lastTemperature - 5;
+                    $maxTemperature = $lastTemperature + 5;
+                    $temperature = rand($minTemperature,$maxTemperature);
+                } else {
+                    switch ($weatherType) {
+                        case "sunny":
+                            $temperature = rand(-50,50);
+                            break;
+                        case "cloudy":
+                            $temperature = rand(-50,15);
+                            break;
+                        case 'rainy':
+                            $temperature = rand(-10,50);
+                            break;
+                        case 'snowy':
+                            $temperature = rand(-50,1);
+                            break;
+                    }
                 }
 
                 ForecastsModel::create([
@@ -51,6 +59,9 @@ class ForecastsSeeder extends Seeder
                     'weather_type' => $weatherType,
                     'probability' => $probability,
                 ]);
+
+                $lastTemperature = $temperature;
+
             }
         }
     }
