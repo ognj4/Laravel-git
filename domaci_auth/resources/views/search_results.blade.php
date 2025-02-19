39172c8ef0be7d3fef4771256e7c84388550bf88
ognj4 <stepanovicognjenos@gmail.com>
2025-02-19 15:38:34 +0100

    * Design layout

--- domaci_auth/resources/views/search_results.blade.php
+++ domaci_auth/resources/views/search_results.blade.php
@@ -2,9 +2,13 @@
 
 @section('content')
 
-
+<div class="d-flex flex-wrap container">
     @foreach($cities as $city)
-        <p>{{ $city->name }}</p>
+        <p>
+            <a class="btn btn-primary text-white m-2" href="{{ route('forecast.permalink', ['city'=> $city->name]) }}">{{ $city->name }}</a>
+        </p>
     @endforeach
+</div>
+
 
 @endsection
--- domaci_auth/resources/views/welcome.blade.php
+++ domaci_auth/resources/views/welcome.blade.php
@@ -4,12 +4,17 @@
 @section('content')
 
 
-    <form method="GET" action="{{ route('forecast.search') }}">
+    <form method="GET" action="{{ route('forecast.search') }}" class="d-flex justify-content-center align-items-center vh-100">
 
-        <div>
-            <input type="text" name="city" placeholder="Unesite ime grada">
+        <div class="container">
+            <h1 class="mb-4" style="color: white">Pronadji grad</h1>
+
+
+            <div class="mb-3">
+                <input type="text" name="city" class="form-control" placeholder="Unesite ime grada" required>
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Pronadji</button>
         </div>
-        <button type="submit">Pronadji</button>
 
     </form>
 
