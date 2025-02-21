d965d784fc40efa0a86874467a6edf6aa2296eec
ognj4 <stepanovicognjenos@gmail.com>
2025-02-21 20:53:50 +0100

    * UserCities migration and model

---
+++ domaci_auth/app/Models/UserCities.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCities extends Model
+{
+    protected $table = 'user_cities';
+
+    protected $fillable = [
+        'user_id', 'city_id'
+    ];
+}
