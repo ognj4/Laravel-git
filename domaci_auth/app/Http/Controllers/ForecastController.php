feea2459918196043e63f2bd0df8f71400ec1fc5
ognj4 <stepanovicognjenos@gmail.com>
2025-02-27 15:04:06 +0100

    * sunrise i sunset added from astronomy api

--- domaci_auth/app/Http/Controllers/ForecastController.php
+++ domaci_auth/app/Http/Controllers/ForecastController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\CitiesModel;
-use App\Models\ForecastsModel;
+use Illuminate\Support\Facades\Http;
 
 class ForecastController extends Controller
 {
     public function index(CitiesModel $city)
     {
-        return view ('forecasts', compact('city'));
+
+        $response = Http::get(env('weather_api_url').'v1/astronomy.json', [
+            'key' => env('WEATHER_API_KEY'),
+            'q' => $city->name,
+            'aqi' =>'no',
+            'days' => 1,
+        ]);
+
+        $jsonResponse = $response->json();
+        $sunrise = $jsonResponse['astronomy']['astro']['sunrise'];
+        $sunset = $jsonResponse['astronomy']['astro']['sunset'];
+
+
+        return view ('forecasts', compact('city', 'sunrise','sunset'));
     }
 }
--- domaci_auth/resources/views/forecasts.blade.php
+++ domaci_auth/resources/views/forecasts.blade.php
@@ -1,3 +1,7 @@
 @foreach($city->forecasts as $prognoza)
+
+    <p>Sunrise u {{ $sunrise }}</p>
+    <p>Sunset u {{ $sunset }}</p>
+
     <p>Datum: {{ $prognoza->forecast_date }} - Temperatura: {{ $prognoza->temperature }}</p>
 @endforeach
